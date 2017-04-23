799b0c785e2e2b37858917e74adb71b3dfd51e69
Dominik <logarrt@gmail.com>
2017-04-23 12:27:13 +0200

    added queries interface

---
+++ src/main/java/com/example/DataBaseSourceConfig.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
+/**
+ * Created by Dominik on 23.04.2017.
+ */
+@Configuration
+public class DataBaseSourceConfig {
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Profile("postgres")
+    public DataSource postgresDataSource(){
+        return DataSourceBuilder.create().build();
+    }
+}
--- src/main/java/com/example/model/LogRepository.java
+++ src/main/java/com/example/model/LogRepository.java
@@ -3,9 +3,24 @@ package com.example.model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Created by Dominik on 22.04.2017.
  */
 @Repository
 public interface LogRepository extends JpaRepository<Log,Integer    > {
+    public Log findById(int id);
+
+    public List<Log> findAllByDescriptionLike(String descriptionPart);
+
+    public List<Log> findAllByModule(String module);
+
+    public List<Log> findAllByDateBetween(String date1, String date2);
+
+    public List<Log> findAllByDescriptionLikeAndModule(String module, String descriptionPart);
+
+    public List<Log> findAllByDescriptionLikeAndDateBetween(String descriptionPart, String date1, String date2);
+
+    public List<Log> findAllByDescriptionLikeAndModuleAndDateBetween(String module, String descriptionPart, String date1, String date2);
 }
